8870f4fdbf74433a077e54ed3c68fb30fcdecae6
kazuikios <slash0juice@gmail.com>
2022-01-25 16:29:23 +0900

    add blockchain struct.

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -31,11 +32,42 @@ func (b *Block) Print() {
 	fmt.Printf("transactions          %s\n", b.transactions)
 }
 
+// Blockchain is blockchain struct.
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+// NewBlockchain is to return new Blockchain struct.
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init hash")
+	return bc
+}
+
+// CreateBlock is to return new Block struct.
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+// Print is print blockchain data.
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s \n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s", strings.Repeat("*", 25))
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	b := NewBlock(34, "string hash")
-	b.Print()
+	blockChain := NewBlockchain()
+	blockChain.CreateBlock(2, "hash 1")
+	blockChain.CreateBlock(7, "hash 2")
+	blockChain.Print()
 }
