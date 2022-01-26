da4ea91e1baa3c1e72b79d6209507bb8974e727a
kazuikios <slash0juice@gmail.com>
2022-01-26 17:56:45 +0900

    change api to send transaction.

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -132,3 +132,24 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		Value:     t.value,
 	})
 }
+
+// TransactionRequest is TransactionRequest struct.
+type TransactionRequest struct {
+	SenderPrivateKey           *string `json:"sender_private_key"`
+	SenderBlockchainAddress    *string `json:"sender_blockchain_address"`
+	RecipientBlockchainAddress *string `json:"recipient_blockchain_address"`
+	SenderPublicKey            *string `json:"sender_public_key"`
+	Value                      *string `json:"value"`
+}
+
+// Validate is to validate request transaction data.
+func (tr *TransactionRequest) Validate() bool {
+	if tr.SenderPrivateKey == nil ||
+		tr.SenderBlockchainAddress == nil ||
+		tr.RecipientBlockchainAddress == nil ||
+		tr.SenderPublicKey == nil ||
+		tr.Value == nil {
+		return false
+	}
+	return true
+}
--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"goblockchain/utils"
 	"goblockchain/wallet"
 	"html/template"
@@ -63,7 +65,27 @@ func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
 func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
-		io.WriteString(w, string(utils.JSONStatus("success")))
+		decoder := json.NewDecoder(req.Body)
+		var t wallet.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			io.WriteString(w, string(utils.JSONStatus("fail")))
+			return
+		}
+		if !t.Validate() {
+			log.Println("ERROR: missing field(s)")
+			io.WriteString(w, string(utils.JSONStatus("fail")))
+			return
+		}
+
+		fmt.Println(*t.SenderPublicKey)
+		fmt.Println(*t.SenderPrivateKey)
+		fmt.Println(*t.SenderBlockchainAddress)
+		fmt.Println(*t.RecipientBlockchainAddress)
+		fmt.Println(*t.Value)
+
+		// io.WriteString(w, string(utils.JSONStatus("success")))
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println("ERROR: Invalid HTTP Method")
