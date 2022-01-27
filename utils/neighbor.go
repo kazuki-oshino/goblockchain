dec5f93a41bda5345d203c39214fbac500abf342
kazuikios <slash0juice@gmail.com>
2022-01-27 11:40:42 +0900

    add inspection tool.

---
+++ utils/neighbor.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+// IsFoundHost is found host.
+func IsFoundHost(host string, port uint16) bool {
+	target := fmt.Sprintf("%s:%d", host, port)
+
+	_, err := net.DialTimeout("tcp", target, 1*time.Second)
+	if err != nil {
+		fmt.Printf("%s %v\n", target, err)
+		return false
+	}
+	return true
+}
+
+// PATTERN is regexp pattern.
+var PATTERN = regexp.MustCompile(`((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3})(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)`)
+
+// FindNeighbors is find neighbors.
+func FindNeighbors(myHost string, myPort uint16, startIP uint8, endIP uint8, startPort uint16, endPort uint16) []string {
+	address := fmt.Sprintf("%s:%d", myHost, myPort)
+
+	m := PATTERN.FindStringSubmatch(myHost)
+	if m == nil {
+		return nil
+	}
+	prefixHost := m[1]
+	lastIP, _ := strconv.Atoi(m[len(m)-1])
+	neighbors := make([]string, 0)
+
+	for port := startPort; port <= endPort; port++ {
+		for ip := startIP; ip <= endIP; ip++ {
+			guessHost := fmt.Sprintf("%s%d", prefixHost, lastIP+int(ip))
+			guessTarget := fmt.Sprintf("%s:%d", guessHost, port)
+			if guessTarget != address && IsFoundHost(guessHost, port) {
+				neighbors = append(neighbors, guessTarget)
+			}
+		}
+	}
+	return neighbors
+}
